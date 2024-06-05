40b76daabee0bb0d38b73496965ff2655d69267f
Abubakar Khan <akhan.bscs22seecs@seecs.edu.pk>
2024-06-05 14:58:12 +0500

    Next.js Portfolio Default

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import Approach from "@/components/Approach";
+import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
+import Hero from "@/components/Hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
+
+export default function Home() {
+  return (
+    <main className="relative bg-black-100 text-white flex justify-center overflow-clip items-center flex-column mx-auto sm:px-10 px-5 dark">
+      <div className="max-w-7xl w-full flex flex-col justify-center">
+        <FloatingNav navItems={navItems} />
+        <Hero />
+        <Grid />
+        <RecentProjects />
+        <Clients />
+        <Experience />
+        <Approach />
+        <Footer />
+      </div>
+    </main>
+  );
+}
---
+++ components/Footer.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa";
+import { socialMedia } from "@/data";
+
+const Footer = () => {
+  return (
+    <footer className="w-full mb-[100px] pb-10 md:mb-5" id="contact">
+      <div className="flex flex-col items-center">
+        <h1 className="heading lg:max-w-[45vw] mb-8">
+          Ready to take <span className="text-purple">your</span> digital
+          presence to the next level?
+        </h1>
+        <p className="mb-10">
+          Reach out to me today and let&apos;s discuss how I can help you
+          achieve your goals.
+        </p>
+        <a href="mailto:abubakarokhan4646@gmail.com">
+          <MagicButton
+            title="Let's get in touch"
+            icon={<FaLocationArrow></FaLocationArrow>}
+            position="right"
+          />
+        </a>
+      </div>
+
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
+        <p className="md:text-base text-sm md:font-normal font-light">
+          Copyright &copy; 2024 Abubakar Khan
+        </p>
+
+        <div className="flex items-center md:gap-3 gap-6">
+          {socialMedia.map((profile) => (
+            <div
+              key={profile.id}
+              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-300 rounded-lg border border-black-300"
+            >
+              <img
+                src={profile.img}
+                alt={profile.id.toString()}
+                width={20}
+                height={20}
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
